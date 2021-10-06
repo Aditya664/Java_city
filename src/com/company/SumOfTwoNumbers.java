184c8c19447a162d4fa1b8cf291ae691f5ea3bca
Aditya <Adityadeshmukh7350@gmail.com>
2021-10-06 03:42:41 -0700

    Initial

--- src/com/company/SumOfTwoNumbers.java
+++ src/com/company/SumOfTwoNumbers.java
@@ -2,7 +2,10 @@ package com.company;
 
 public class SumOfTwoNumbers {
     public static void main(String[] args) {
-
+        int a =30;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Addition of " +a+ " And " +b+ " is " +sum);
     }
 
 }
